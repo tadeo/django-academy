c0e3482ffc8981e4cc0be3f91373165d37b81af9
Gerardo <pituuy@yahoo.com.ar>
2012-05-23 23:31:39 -0300

    Add to views subscribe

--- django_academy/apps/main/views.py
+++ django_academy/apps/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response('home.html')
+
+def subscribe(request):
+    return render_to_response('subscribe.html')
