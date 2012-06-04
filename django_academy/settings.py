0f0a0c53e4828ce91143ce9a72f1210e70e9d144
Marcos Irureta Goyena <marcos.irureta.goyena@gmail.com>
2012-06-04 19:34:48 -0300

    Fix settings.py

--- django_academy/settings.py
+++ django_academy/settings.py
@@ -99,8 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',    
+    'django.contrib.messages.middleware.MessageMiddleware', 
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
