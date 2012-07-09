da072461813b90eba95264468db3dd2fa76166d8
Gerardo <pituuy@yahoo.com.ar>
2012-07-09 19:41:41 -0300

    Settings modified

--- django_academy/settings.py
+++ django_academy/settings.py
@@ -127,7 +127,7 @@ INSTALLED_APPS = (
     'apps.main',
     'south',
 )
-LOGIN_URL = '/admin/'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
