311d9827bb5a3ff6705f71d5296c48e1a63ce545
fabian <famole@gmail.com>
2012-07-23 21:54:51 -0300

    Add listCourses view and url configuration.

--- django_academy/urls.py
+++ django_academy/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
      url(r'^subscribers/$', ListView.as_view(model=Subscriber, paginate_by=3), name='subscribers'),
      url(r'^subscribers/(?P<pk>\d+)$', SubscriberDetailView.as_view(model=Subscriber), name='subscriber_detail'),
     # url(r'^django_academy/', include('django_academy.foo.urls')),
+     url(r'^courses/$', 'apps.main.views.listCourses', name='listCourses'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
