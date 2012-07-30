48874cdff1930e7f2339cbd5b56a66992c4e5f29
maui <niko@mauitz.com>
2012-07-30 20:52:34 -0300

    add grapelli app for admin layout

--- django_academy/settings.py
+++ django_academy/settings.py
@@ -123,6 +123,7 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    'grappelli',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- django_academy/urls.py
+++ django_academy/urls.py
@@ -21,9 +21,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    
+    (r'^grappelli/', include('grappelli.urls')),
 )
-
 
 if settings.DEBUG:
     urlpatterns += patterns('',
